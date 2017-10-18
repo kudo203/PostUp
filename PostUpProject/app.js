d0f3e4643488ac5ac3902dd5f89c12b56349bfb0
Koosh Doctor <koosh.20@gmail.com>
2017-10-17 23:45:23 -0400

    Functionality of adding a post, upvoting, and adding links

--- PostUpProject/app.js
+++ PostUpProject/app.js
@@ -1,24 +1,21 @@
-/*
-var app = angular.module('PostUp', []);
-
-app.controller('MainCtrl', [
-    '$scope',
-    function($scope){
-        $scope.test = 'Hello world!';
-    }]);
-
-*/
 
 var app = angular.module('PostUp',[]);
 app.controller('MainCtrl',[
     '$scope',
     function($scope){
-        $scope.test = 'hello world!';
-        $scope.posts = [
-            {title: 'post 1', upvotes: 5},
-            {title: 'post 2', upvotes: 2},
-            {title: 'post 3', upvotes: 15},
-            {title: 'post 4', upvotes: 9},
-            {title: 'post 5', upvotes: 4}
-        ]
-    }]);
+        $scope.posts = []
+        $scope.addPost = function () {
+            if($scope.title==''){return;}
+            $scope.posts.push({
+                title:$scope.title,
+                link:$scope.link,
+                upvotes: 0})
+            $scope.title = '';
+            $scope.link = '';
+        };
+        
+        $scope.incrementUpvote = function (post) {
+            post.upvotes +=1;
+        }
+    }
+]);
