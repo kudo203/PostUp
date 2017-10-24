92fe8cec55fa94ab5daba68052983e1406ce1883
Koosh Doctor <koosh.20@gmail.com>
2017-10-24 03:16:04 -0400

    Added a ui-router and default home for bad url request

--- PostUpProject/app.js
+++ PostUpProject/app.js
@@ -1,5 +1,19 @@
 
-var app = angular.module('PostUp',[]);
+var app = angular.module('PostUp',['ui.router']);
+app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function ($stateProvider,$urlRouterProvider) {
+        $stateProvider
+            .state('home',{
+                url:'/home',
+                templateUrl: '/home.html',
+                controller: 'MainCtrl'
+                });
+        $urlRouterProvider.otherwise('home');
+    }]);
+
+
 app.factory('posts',[function () {
     var set = {
         posts:[]
