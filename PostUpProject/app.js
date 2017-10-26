0a94ae5955dd38c904f516c9cefa37cb0e7a4f13
Koosh Doctor <koosh.20@gmail.com>
2017-10-26 00:16:03 -0400

    Added a page specific to a post and made the comments available

--- PostUpProject/app.js
+++ PostUpProject/app.js
@@ -9,34 +9,59 @@ app.config([
                 url:'/home',
                 templateUrl: '/home.html',
                 controller: 'MainCtrl'
-                });
+                })
+            .state('posts',{
+                url:'/posts/{id}',
+                templateUrl: '/posts.html',
+                controller: 'PostsCtrl'
+            })
         $urlRouterProvider.otherwise('home');
-    }]);
-
-
-app.factory('posts',[function () {
+    }])
+    .factory('posts',[function () {
     var set = {
         posts:[]
     };
     return set;
-}])
-app.controller('MainCtrl',[
-    '$scope',
-    'posts',
-    function($scope,posts){
-        $scope.posts = posts.posts;
-        $scope.addPost = function () {
-            if($scope.title==''){return;}
-            $scope.posts.push({
-                title:$scope.title,
-                link:$scope.link,
-                upvotes: 0})
-            $scope.title = '';
-            $scope.link = '';
-        };
+    }])
+    .controller('MainCtrl',[
+        '$scope',
+        'posts',
+        function($scope,posts){
+            $scope.posts = posts.posts;
+            $scope.addPost = function () {
+                if($scope.title==''){return;}
+                $scope.posts.push({
+                    title:$scope.title,
+                    link:$scope.link,
+                    upvotes: 0,
+                    comments: [
+                        {author: 'Joe', body: 'Cool post!', upvotes: 0},
+                        {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+                    ]});
+                $scope.title = '';
+                $scope.link = '';
+            }
 
         $scope.incrementUpvote = function (post) {
             post.upvotes +=1;
         }
-    }
-]);
+    }])
+    .controller('PostsCtrl',[
+        '$scope',
+        '$stateParams',
+        'posts',
+        function ($scope,$stateParams,posts) {
+            $scope.post = posts.posts[$stateParams.id];
+            $scope.addComment = function () {
+                if($scope.body==''){return;}
+                $scope.post.comments.push({
+                    body:$scope.body,
+                    author:'author',
+                    upvotes: 0
+                    });
+                $scope.body = '';
+            }
+            $scope.incrementUpvotes = function (comment) {
+                comment.upvotes +=1;
+            }
+        }]);
